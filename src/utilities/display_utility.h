12ab33a9e68eadc6011629a6550bbaa8325d8174
Kevin Newman <kevin@kevinnewman.ca>
2026-01-22 11:30:51 -0500

    Gameplay intro touches

--- src/utilities/display_utility.h
+++ src/utilities/display_utility.h
@@ -16,6 +16,10 @@ void draw_boss_health_bar(const char *name, float ratio, float flash);
 // Simple player health bar API (smaller, bottom-right positioned)
 void draw_player_health_bar(const char *name, float ratio, float flash);
 
+// UI intro animation controls (0.0 = off-screen, 1.0 = fully visible)
+void display_utility_set_boss_ui_intro(float progress);
+void display_utility_set_player_ui_intro(float progress);
+
 void display_utility_solid_black_transition(bool fadeIn, float speed);
 
 #endif
